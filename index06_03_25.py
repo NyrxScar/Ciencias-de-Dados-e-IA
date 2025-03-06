055b411960513419d41102f78413f7ff98a9e168
JOÃO VINÍCIUS DE AQUINO FARIAS <joao_va_farias@estudante.sesisenai.org.br>
2025-03-06 10:42:45 -0300

    CDIA

---
+++ index06_03_25.py
@@ -0,0 +1,12 @@
+minutos = 700
+horas = minutos/60
+print (minutos, "minutos corresponde a", horas, "horas")
+minutos = 500
+horas = minutos/60
+print (minutos, "minutos corresponde a", horas, "horas")
+minutos = 700
+horas = minutos//60
+print (minutos, "minutos corresponde a", horas, "horas")
+minutos = 500
+horas = minutos//60
+print (minutos, "minutos corresponde a", horas, "horas")
