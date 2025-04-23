ad72bce6701e1dd4222446635327da9438257732
NYRX OLIVEIRA DE AQUINO FARIAS <nyrx_farias@estudante.sesisenai.org.br>
2025-04-23 11:54:58 -0300

    add

--- Aulas Algoritimo e Programação/Aula e Anotações/Aula17_04_25/aula17_04_25.py
+++ Aulas Algoritimo e Programação/Aula e Anotações/Aula17_04_25/aula17_04_25.py
@@ -18,6 +18,30 @@
 #     print(lista)
 
 import pandas as pd
+pd.read_excel("/content/controle_de_abacates.xlsx")
+
+
+
+# lista = ['O carro', 'O peixe', 123, 111]
+# nova_lista = ['pedra',lista]
+# print(lista+nova_lista)
+
+# # livros = ['Java', 'SqlServer', 'Delphi', 'Python']
+# # livros.append("Android")
+# # print(livros)
+# # livros.insert('Oracle', 0)
+# # print(livros)
+
+
+
+# lista = []
+# for x in range(5):
+#     n = int(input("Digite um número inteiro: "))
+#     lista.append(n)
+#     lista.sort()
+#     print(lista)
+
+import pandas as pd
 planilhas_abacate = pd.read_excel("Aulas Algoritimo e Programação\Aula e Anotações\Aula17_04_25\controle_de_abacates.xlsx")
 print(planilhas_abacate['preco_medio'])
 print(50*"")
---
+++ Aulas Algoritimo e Programação/Aula e Anotações/Aula23_04_25/index.py
@@ -0,0 +1,118 @@
+# Lista = [ 15, 7, 27, 39]
+# procurado = int(input("Digite o número que vc quer encontrar a na sua lista: "))
+# achou = False
+# x = 0
+# adicionarProcurado = procurado.append
+# while x<len(Lista):
+#     if Lista[x] == procurado:
+#         achou = True
+#         break
+#     x+=1
+# if achou:
+#     print(f"{procurado} achado na posição x")
+# else:
+#     print(f"{procurado} não foi achado na lista, deseja adicionar na sua lista? ")
+#     if "sim" or "Sim":
+#         adicionarProcurado
+#         print(f" Seu item {adicionarProcurado} foi adicionado na Lista")
+# #######################################
+# Lista = [15, 7, 27, 39]
+# procurado = int(input("Digite o número que você quer encontrar na sua lista: "))
+# achou = False
+# x = 0
+# adicionarProcurado = procurado.append  # A função append precisa ser chamada corretamente
+# while x < len(Lista):
+#     if Lista[x] == procurado:
+#         achou = True
+#         break
+#     x += 1
+
+# if achou:
+#     print(f"{procurado} achado na posição {x}")
+# else:
+#     print(f"{procurado} não foi encontrado na lista, deseja adicionar na sua lista? ")
+#     resposta = input()  # A entrada do usuário é lida aqui
+#     if resposta == "sim" or resposta == "Sim":  # Verifica se a resposta é "sim" ou "Sim"
+#         Lista.append(procurado)  # Agora o número é adicionado à lista
+#         print(f"Seu item {procurado} foi adicionado na lista.")
+#     else:
+#         print("O item não foi adicionado.")
+
+################################################
+
+# Lista = [ 15, 7, 27, 39]
+# procurado = int(input("Digite o número que vc quer encontrar a na sua lista: "))
+# achou = False
+# x = 0
+# index = []
+# for lista in Lista:
+#         if lista == procurado:
+#               print(f"{lista} achado na posição {x}")
+#               break
+#         x+=1
+# if not achou:
+#        print(f"O número {procurado} não foi encontrado na lista")
+
+###################################################################
+
+# achou = False
+# x = 0
+# procurado = int(input("Digite o número que vc quer encontrar a na sua lista: "))
+# Lista = [ 15, 7, 27, 39]
+# for elementos in Lista:
+#    if elementos == procurado:
+#      print(f"{Lista} achado na posição {x}")
+#      break
+#    x+=1
+# if not achou:
+#         print(f"O número {procurado} não foi encontrado na lista")
+########################################
+
+# Lista = [1, 7, 2, 4, 0, 2]
+# maximo = Lista[0]
+# minimo = Lista[0]
+# for encontrar in Lista:
+#     if encontrar>maximo:
+#         maximo = encontrar
+#     if encontrar<minimo:
+#         minimo = encontrar
+# print(f"O minimo é {minimo} e o maximo é {maximo}")
+# Valores = [9,3,6,0]
+# pares = []
+# impares = []
+# for elementos in Valores:
+#     if elementos%2==0:
+#         pares.append(elementos)
+#     else:
+#         impares.append(elementos)
+
+# print(pares)
+# print(impares)
+
+# Listanomes = []
+# nomes = input("Escreva os 15 nomes: ")
+# for Listanomes in range(len(Listanomes(1,16))):
+#     print(Listanomes, end = ",")
+
+########################################
+
+achou = False
+Listanomes = ["Delphir", "Delphir2", "Delphir3", "Delphir4", "Delphir5", "Delphir6", "Delphir7","Delphir8","Delphir9","Delphir10", "Delphir11", "Delphir12", "Delphir13", "Delphir14", "Delphir15"]
+procurado = str(input("Digite o nome que vc quer encontrar na lista: "))
+for elementos in Listanomes:
+    if elementos == procurado:
+        print(f"{procurado} está na lista")
+        
+if not achou:
+    print(f"Esse nome {procurado} não está na lista")
+
+# x = 0
+# procurado = int(input("Digite o número que vc quer encontrar a na sua lista: "))
+# Lista = [ 15, 7, 27, 39]
+# for elementos in Lista:
+#    if elementos == procurado:
+#      print(f"{Lista} achado na posição {x}")
+#      break
+#    x+=1
+# if not achou:
+#         print(f"O número {procurado} não foi encontrado na lista")
